5d858ccace1ba324191a937b2cfc9cfaff28991f
Bill Cauchois <wcauchois@gmail.com>
2022-04-10 14:07:01 -0400

    print query string params

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -3,7 +3,9 @@ import { Handler } from "@netlify/functions"
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: "hello world",
+    body: `hello world. my query parms are: ${JSON.stringify(
+      event.queryStringParameters
+    )}`,
   }
 }
 
